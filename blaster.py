81278c4da39de366ea3c1255cfdde12969094e00
snacktavish <ejmctavish@gmail.com>
2013-12-12 15:48:19 -0600

    messed up convert

--- blaster.py
+++ blaster.py
@@ -2,37 +2,40 @@ import sys
 from subprocess import call
 from Bio.Blast.Applications import NcbiblastnCommandline
 from Bio.Blast import NCBIXML
-import dendropy
+
 
 #infi=sys.argv[1] #query info should be in non interleaved nexus format
 #ancseqs=sys.argv[2] #test sequences should be in fasta format
 
-allseq=open("rbcL_noEPI.nex").readlines()
-treseq=open("rbcL_noEPI_subA.nex").readlines()
+allseq=open("rbcL.nex").readlines()
+treseq=open("rbcL_sub2.nex").readlines()
 
 
 nams=set()
-for lin in treseq[6:-2]:
-   nams.add(lin.split()[0])
+for lin in treseq[6:-3]:
+   nams.add(lin.split('\t')[0])
 
 
-identdict={}
 testseqs=[]
 for lin in allseq[6:-3]:
    if lin.split('\t')[0] not in nams:
-     testseqs.append(lin.split('\t'))
-     identdict[lin.split('\t')[0]]={}
+     testseqs.append(lin)
 
+
+seqnams=set()
+identdict={}
 
 #for i,lin in enumerate(open(infi)):
 #   if i>=6:
-for lin in testseqs:
-      nam=lin[0]
-      assert(nam in identdict)
+for item in testseqs:
+      nam=lin.split()[0]
+      if nam not in identdict:
+            identdict[nam]={}
+      seqnams.add(nam)
       fi=open("query.txt",'w')
-      fi.write(lin[1]) 
+      fi.write(lin.split()[1]) 
       fi.close()
-      for i in range(1,11):
+      for i in range(1,2):
         blastn_run = NcbiblastnCommandline(query="query.txt", subject="ancestorT{}.fasta".format(i), evalue=0.001, outfmt=5, out="test.xml")
         stdout, stderr = blastn_run()
         result_handle = open("test.xml")
@@ -51,8 +54,5 @@ for nam in identdict:
     for node in identdict[nam]:
          sumdict.append((sum(identdict[nam][node]),node))
     sumdict.sort()
-    print(nam,sumdict[-1])
-    tlst = dendropy.Tree.get_from_path('test.tre', "newick")
-    tlst.find_node_with_label(sumdict[-1]).new_child(taxon=dendropy.dataobject.taxon.Taxon(taxon=None, label=nam, oid=None))
-
-
+    print(nam,sumdict[0])
+  
--- convert.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-        for i in `seq 1 10`; do
-            echo item: $i
-            python convertformat.py -i nexus fasta ancestorT${i}.nex
-        done
