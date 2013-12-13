22f3cd858f4be939e9072ca3e969852113c99f83
Emily Jane McTavish <ejmctavish@gmail..com>
2013-12-13 15:39:44 -0600

    added dendropy actions

--- blaster.py
+++ blaster.py
@@ -2,13 +2,13 @@ import sys
 from subprocess import call
 from Bio.Blast.Applications import NcbiblastnCommandline
 from Bio.Blast import NCBIXML
+import dendropy
 
-#WTFFFFFF
 #infi=sys.argv[1] #query info should be in non interleaved nexus format
 #ancseqs=sys.argv[2] #test sequences should be in fasta format
 
-allseq=open("rbcL.nex").readlines()
-treseq=open("rbcL_sub2.nex").readlines()
+allseq=open("rbcL_noEPI.nex").readlines()
+treseq=open("rbcL_noEPI_1.nex").readlines()
 
 
 nams=set()
@@ -25,17 +25,16 @@ for lin in allseq[6:-3]:
 seqnams=set()
 identdict={}
 
-#for i,lin in enumerate(open(infi)):
-#   if i>=6:
 for item in testseqs:
-      nam=lin.split()[0]
+      nam=item.split()[0]
       if nam not in identdict:
             identdict[nam]={}
       seqnams.add(nam)
       fi=open("query.txt",'w')
-      fi.write(lin.split()[1]) 
+      fi.write(item.split()[1]) 
       fi.close()
       for i in range(1,2):
+        call(["python","convertformat.py","-i","nexus","fasta","ancestorT{:d}.nex".format(i)])
         blastn_run = NcbiblastnCommandline(query="query.txt", subject="ancestorT{}.fasta".format(i), evalue=0.001, outfmt=5, out="test.xml")
         stdout, stderr = blastn_run()
         result_handle = open("test.xml")
@@ -54,5 +53,17 @@ for nam in identdict:
     for node in identdict[nam]:
          sumdict.append((sum(identdict[nam][node]),node))
     sumdict.sort()
-    print(nam,sumdict[0])
-  
+    print(nam,sumdict[-1])
+
+
+t1=dendropy.Tree.get_from_path("rep_1.tre", "newick")  
+nad=t1.find_node_with_label(sumdict[-1][-1])
+#.new_child(taxon=dendropy.Taxon(label=nam),edge_length=0.15)
+nad.edge.length
+
+
+t1.write(open("addition.tre",'w'),schema="nexus")
+
+call(["Garli","score.conf"])
+
+
